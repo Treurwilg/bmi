2324c688c3d4c112edeb36e2c09ccf685019bff4
janKooijman <jcz.kooijman@hetnet.nl>
2014-03-28 19:27:38 +0100

    First commit for working version

---
+++ js/bmi1.js
@@ -0,0 +1,15 @@
+/** bmi.js
+*/
+"use strict";
+console.log("load bmi1.js");
+function bmi() {
+	console.log("called bmi()");
+	var sLengte = document.getElementById("lengte").value;
+	console.log("lengte " + sLengte);
+	var sGewicht =	document.getElementById("gewicht").value;
+	var lengte = parseFloat(sLengte)/100;
+	var gewicht = parseFloat(sGewicht);
+	var bmi = gewicht / lengte / lengte;
+	var veld = document.getElementById("veld");
+	veld.innerHTML = "je BMI is: " + bmi.toPrecision(4);
+}
